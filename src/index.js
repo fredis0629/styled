e59908708030020e5411d64a28096db89791249e
fredis0629 <fredis0629@gmail.com>
2019-07-24 15:09:19 +0300

    past test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function styled() {
-
+module.exports = function styled(startObj = null) {
+  return function (strArr,...functs) {
+    return function (objArgs) {
+      let result = '';
+      if(startObj != null) result = startObj(objArgs);
+      result += strArr[0] || '' ;
+      if(functs.length > 0) {
+        functs.forEach((val, index) =>{
+          result += (objArgs ? val(objArgs) : val({})) +strArr[index+1]
+      })}
+      return result.replace(/\n|\t/g, '');
+    }
+  };
 }
