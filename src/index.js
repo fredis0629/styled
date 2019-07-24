9acb611e065203ce48e003a53517bae3383082a0
fredis0629 <fredis0629@gmail.com>
2019-07-24 17:54:24 +0300

    second try

--- src/index.js
+++ src/index.js
@@ -1,14 +1,44 @@
-module.exports = function styled(startObj = null) {
-  return function (strArr,...funcs) {
-    return function (objArgs) {
+
+module.exports = function styled(initExpresion = null) {
+  // receive initExpresion
+  return function (strings,...functionExpressions) { // returns function that parses template strings
+    return function (objArgs) { // returns function that build the resulting string
       let result = '';
-      if(startObj != null) result = startObj(objArgs);
-      result += strArr[0] || '' ;
-      if(funcs.length > 0) {
-        funcs.forEach((val, index) =>{
-          result += (objArgs ? val(objArgs) : val({})) +strArr[index+1]
-      })}
-      return result.replace(/\n|\t/g, '');
+
+      if(initExpresion != null) result = initExpresion(objArgs); // discharge initExpresion if necessary
+      result += strings[0] || '' ; // adds first string element from template (if exist) to result string variable
+      // if functionExpressions
+      if(functionExpressions.length) {
+        //if functionExpressions were passed assemble discharged functionExpressions and template strings to result string
+        functionExpressions.forEach((expression, index) => {
+          result += (objArgs ? expression(objArgs) : expression({})) + strings[index+1]
+        })
+      }
+      return result.replace(/\n|\t/g, ''); // remove newline and tab characters from result string and return it as answer
+    }
+  };
+}
+
+
+module.exports = function styled(initExpresion = null) {
+  // receive initExpresion
+  return function (strings,...functionExpressions) {  // returns function that parses template strings
+    return function (objArgs) { // returns function that build the resulting string
+      let initialString = '';
+
+      if(initExpresion != null) initialString = initExpresion(objArgs); // discharge initExpresion if necessary
+      initialString += strings[0] || '' ; // adds first string element from template (if exist) to initialString
+
+      // callback function for reduce that assembles discharged functionExpressions and template strings to result string
+      function cbForReduce(acc, expression, index) {
+        return acc += (objArgs ? expression(objArgs) : expression({})) +strings[index+1];
+      }
+      // build result string
+      let result = (functionExpressions.length) ?
+        functionExpressions.reduce( cbForReduce ,initialString) // assemble discharged functionExpressions and template strings to result string with initialString as initialValue for reduce
+        : initialString; // if there now functionExpressions assign initialString to result string
+
+      return result.replace(/\n|\t/g, ''); // remove newline and tab characters from result string and return it as answer
     }
   };
 }
